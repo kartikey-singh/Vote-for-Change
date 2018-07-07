861918e3ddfab8bf93d1e2c3edf8a2cf4c2eda5c
Kartikey Singh <kartikey21@rediffmail.com>
2018-07-07 23:07:10 +0530

    second deploy

--- vote/settings.py
+++ vote/settings.py
@@ -47,10 +47,9 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    # 'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'vote.urls'
