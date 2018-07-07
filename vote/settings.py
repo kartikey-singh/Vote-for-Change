6d92926526dd851fdada7a2e95c82e81096d0865
Kartikey Singh <kartikey21@rediffmail.com>
2018-07-07 23:23:36 +0530

    second deploy

--- vote/settings.py
+++ vote/settings.py
@@ -131,8 +131,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
