569caf45d5eb9f08d1c77478e0147c9d24e603ba
Kartikey Singh <kartikey21@rediffmail.com>
2018-07-08 00:03:25 +0530

    deploy

--- vote/settings.py
+++ vote/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f+@a9+yqeu-amy0rsd9g7f9$#^9^yt6_bg^ei-(hs)ihcv34(r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','voteforchange.herokuapp.com']
 
--- voteapp/views.py
+++ voteapp/views.py
@@ -48,16 +48,16 @@ def contact(request):
 # 	return render(request, 'registration/registration.html')	
 def underconstruction(request):
 	return render(request, 'under-construction.html')	
-def handler404(request):
-    response = render_to_response('404.html', {},context_instance=RequestContext(request))
-    response.status_code = 404
-    return response
+# def handler404(request):
+#     response = render_to_response('404.html', {},context_instance=RequestContext(request))
+#     response.status_code = 404
+#     return response
 
 
-def handler500(request):
-    response = render_to_response('500.html', {},context_instance=RequestContext(request))
-    response.status_code = 500
-    return response
+# def handler500(request):
+#     response = render_to_response('500.html', {},context_instance=RequestContext(request))
+#     response.status_code = 500
+#     return response
 
 @login_required(login_url = "/login")
 def DashboardView(request):
